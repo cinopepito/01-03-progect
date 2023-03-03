d040d0bbeaede815f46f6c389299e30e77715a04
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-03-03 11:12:42 +0100

    room available

--- object.js
+++ object.js
@@ -1,10 +1,12 @@
 let hotel = {
-    name:'Quay',
-    rooms:40,
-    booked:25,
-    checkAvailability:function(){
-        return this.rooms-this.booked;
-    }
-}
-let holtelName = hotel.name;
-console.log(holtelName)
+  name: "Quay",
+  rooms: 40,
+  booked: 25,
+  checkAvailability: function () {
+    return this.rooms - this.booked;
+  },
+};
+
+let restRoom = document.getElementById("rest");
+restRoom.style.color='orange';
+restRoom.textContent = hotel.checkAvailability() + ' rooms available';
